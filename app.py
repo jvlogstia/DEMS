44992da1c965c2ea64c549be5f99e8daf5b45a02
jvlogstia <95112627+jvlogstia@users.noreply.github.com>
2025-09-24 19:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def close_db_connection(exception=None):
 # Run App
 # -----------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000, debug=False)
